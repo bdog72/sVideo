f9969b814f8b85940def677cbdc8e2287e3d4511
Brian Bycynski <high5bri@hotmail.com>
2018-02-21 10:29:08 -0500

    added state constructor

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import preload from '../data.json';
 
 import ShowCard from './ShowCard';
 
-const Search = () => (
-    <div className="search">
-        {preload.shows.map(show => <ShowCard show={show} />)}
-    </div>
-);
+class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchTerm: 'This is some sort of debug statement'
+        };
+        this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+    }
+
+    handleSearchTermChange(event) {
+        this.setState({ searchTerm: event.target.value });
+    }
+
+    render() {
+        return (
+            <div className="search">
+                <header>
+                    <h1>sVideo</h1>
+                    <input
+                        onChange={this.handleSearchTermChange}
+                        value={this.state.searchTerm}
+                        type="text"
+                        placeholder="Search"
+                    />
+                </header>
+                <div>
+                    {preload.shows.map(show => (
+                        <ShowCard key={show.imdbID} {...show} />
+                    ))}
+                </div>
+            </div>
+        );
+    }
+}
+
 export default Search;
