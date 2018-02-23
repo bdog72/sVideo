c061f912b60a50e8f56d748577b0b7a8f19fbf19
Brian Bycynski <high5bri@hotmail.com>
2018-02-23 09:45:33 -0500

    added hot module replacement

--- js/Search.jsx
+++ js/Search.jsx
@@ -4,23 +4,19 @@ import preload from '../data.json';
 import ShowCard from './ShowCard';
 
 class Search extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            searchTerm: 'This is some sort of debug statement'
-        };
-        this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
-    }
+    state = {
+        searchTerm: ''
+    };
 
-    handleSearchTermChange(event) {
+    handleSearchTermChange = event => {
         this.setState({ searchTerm: event.target.value });
-    }
+    };
 
     render() {
         return (
             <div className="search">
                 <header>
-                    <h1>sVideo</h1>
+                    <h1>{this.state.searchTerm}</h1>
                     <input
                         onChange={this.handleSearchTermChange}
                         value={this.state.searchTerm}
@@ -29,9 +25,16 @@ class Search extends Component {
                     />
                 </header>
                 <div>
-                    {preload.shows.map(show => (
-                        <ShowCard key={show.imdbID} {...show} />
-                    ))}
+                    {preload.shows
+                        .filter(
+                            show =>
+                                `${show.title} ${show.description}`
+                                    .toUpperCase()
+                                    .indexOf(
+                                        this.state.searchTerm.toUpperCase()
+                                    ) >= 0
+                        )
+                        .map(show => <ShowCard key={show.imdbID} {...show} />)}
                 </div>
             </div>
         );
